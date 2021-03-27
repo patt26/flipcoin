8bac2b94f0540fef91486acdb93b78017841555d
patt26 <kharchepratik123@gmail.com>
2021-03-27 14:12:36 +0530

    Added Display winner

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,7 +1,23 @@
 #!/bin/bash -x
 
-coin=$((RANDOM%2))
-	if [ $((RANDOM%2)) -eq 1 ]
-		then echo HEAD
-	else echo TAIL
+heads=0
+tails=0
+count=0
+
+
+	while [ $count -ne 11 ] 
+	do
+
+         	num=$((RANDOM%2))
+	    if [ $num -eq 1 ]
+
+	then ((heads++)) 
+
+	else ((tails++))
 	fi
+	((count++))
+	done
+
+
+echo heads=$heads
+echo tails=$tails
