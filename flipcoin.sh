7e3869d9fe0984458e5ebad6966f9cd6fc7250e1
patt26 <kharchepratik123@gmail.com>
2021-03-27 16:12:38 +0530

    Added Win or lose

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,23 +1,39 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 heads=0
 tails=0
-count=0
+counthead=0
+counttail=0
+
+while [[ $counthead -le 21 || $counttail -le 21 ]]
+do
+
+	num=$((RANDOM%2))
+if [ $num -eq 21 ]
+then ((counthead++))
+else ((counttail++))
+fi
+if [ $counthead -eq 21 ]
+then echo Heads win
+elif [ $counttail -eq 21 ]
+then echo Tails win
+break
+fi
+
+done
+
+echo "Total Heads Won $counthead"
+echo "Total Tails Won $counttail"
+
+if [ $counthead -gt $counttail ]
+then difference1= $(($counthead - $counttail))
+echo "Heads won by $difference1"
+
+elif [ $counttail -gt $counhead ]
+then difference2= $(( $counttail - $counthead ))
+echo "Tails won by $difference2"
+
+else echo "Its tie"
+fi
 
 
-	while [ $count -ne 11 ] 
-	do
-
-         	num=$((RANDOM%2))
-	    if [ $num -eq 1 ]
-
-	then ((heads++)) 
-
-	else ((tails++))
-	fi
-	((count++))
-	done
-
-
-echo heads=$heads
-echo tails=$tails
